2f65df757c5f8deb898fac6ca6b80a8d683c4cd9
xingchen <1144290241@qq.com>
2014-08-01 23:43:07 +0800

    改动： 将0.png放到了icon文件夹下面，同时改名为trayIcon.png 完善了Tary类，为它添加了菜单（退出和显示查找对话框），并将发射两个消息：        showDlgSignal()、exitSignal()

--- Tray.cpp
+++ Tray.cpp
@@ -1,8 +1,51 @@
 #include "Tray.h"
+
+#include <QGuiApplication>
+
 #include <QIcon>
+#include <QMenu>
+#include <QAction>
+
 Tray::Tray(QObject *parent) :
     QSystemTrayIcon(parent)
 {
-    QIcon icon(":/0.png");
-    setIcon(icon);
+    QIcon icon("icon\\trayIcon.png");
+    this->setIcon(icon);
+    this->setToolTip("AltRun");
+
+    createContextMenu();
+
+    connect ( this,SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
+              this,SLOT(activatedSlot(QSystemTrayIcon::ActivationReason)) );
+}
+
+void Tray::createContextMenu ()
+{
+    menu = new QMenu();
+
+    exitAction = new QAction( tr("退出"),this );
+    showDlgAction = new QAction( tr("显示查找对话框"),this );
+
+    connect( exitAction,SIGNAL(triggered()),this,SLOT(exitSlot()) );
+    connect( showDlgAction,SIGNAL(triggered()),this,SLOT(showDlgSlot()) );
+
+    menu->addAction(showDlgAction);
+    menu->addAction(exitAction);
+
+    setContextMenu(menu);
+}
+
+void Tray::activatedSlot(QSystemTrayIcon::ActivationReason reason)
+{
+    if( reason == QSystemTrayIcon::Trigger )
+        emit (showDlgSignal () );
+}
+
+void Tray::exitSlot ()
+{
+    emit ( exitSignal() );
+}
+void Tray::showDlgSlot ()
+{
+    emit ( showDlgSignal () );
 }
--- Tray.h
+++ Tray.h
@@ -3,16 +3,31 @@
 
 #include <QSystemTrayIcon>
 
+#include <QAction>
+#include <QMenu>
+
 class Tray : public QSystemTrayIcon
 {
     Q_OBJECT
 public:
     explicit Tray(QObject *parent = 0);
 
+private:
+    QMenu * menu;
+
+    QAction * exitAction;
+    QAction * showDlgAction;
+
+    void createContextMenu();
 signals:
+    // 当显示菜单
+    void showDlgSignal();
+    void exitSignal();
+private slots:
+    void exitSlot();
+    void showDlgSlot();
 
-public slots:
-
+    void activatedSlot(QSystemTrayIcon::ActivationReason);
 };
 
 #endif // TRAY_H
